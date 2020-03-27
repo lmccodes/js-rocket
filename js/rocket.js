7ac24b3ce37c52f2e89122a4c083498249d39d5a
lmccodes <lisa.m.childress@gmail.com>
2020-03-26 22:44:02 -0700

    done lots

--- js/rocket.js
+++ js/rocket.js
@@ -7,22 +7,25 @@ var countdownNumber = 10;
 
   if (state == 2) {
     timer = setInterval(function () {
+        countdownNumber = countdownNumber-1;
         document.getElementById('countdown').innerHTML = countdownNumber;
-        countdownNumber = countdownNumber -1;
-      if (countdownNumber <=0) {
+      if (countdownNumber <= 0) {
         changeState(3);
       };
-    }, 1000);
- } else if (changeState == 3) {
-    var success = setTimeout(function ()
-    {
-      var randomNumber = Math.round(Math.random()*10);
-console.log('random number: ', randomNumber);
-      if (randomNumber > 5) {
-        changeState(4); //success
-      } else {
-        changeState(5); //fail
-      }
-    }, 2000);
- }
+    }, 500);
+ } else if (state == 3) {
+		var success = setTimeout(function () {
+				var randomNumber = Math.round(Math.random()*10);
+
+				console.log('randomNumber:', randomNumber)
+
+				// succes
+				if (randomNumber > 2) {
+					changeState(4);
+
+				} else {
+					changeState(5); // oh no!
+				}
+		}, 2000);
+	};
 }
