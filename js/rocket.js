a7497edc72d02555328b51686ff49666fdd2bcf9
lmccodes <lisa.m.childress@gmail.com>
2020-04-18 05:44:42 -0700

    changed lots of stuff

--- js/rocket.js
+++ js/rocket.js
@@ -7,25 +7,23 @@ var countdownNumber = 10;
 
   if (state == 2) {
     timer = setInterval(function () {
-        countdownNumber = countdownNumber-1;
+        countdownNumber = countdownNumber - 1;
         document.getElementById('countdown').innerHTML = countdownNumber;
+
       if (countdownNumber <= 0) {
         changeState(3);
       };
     }, 500);
  } else if (state == 3) {
-		var success = setTimeout(function () {
-				var randomNumber = Math.round(Math.random()*10);
-
-				console.log('randomNumber:', randomNumber)
-
-				// succes
-				if (randomNumber > 2) {
-					changeState(4);
-
-				} else {
-					changeState(5); // oh no!
-				}
+		var success = setTimeout(function ()
+    {
+			var randomNumber = Math.round(Math.random()*10);
+      console.log('randomNumber:', randomNumber);
+      if (randomNumber > 5) {
+        changeState(4);
+      } else {
+        changeState(5);  //oh, no
+      }
 		}, 2000);
 	};
 }
